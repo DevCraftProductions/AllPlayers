fd32a7082104b662ad4e8883d7a67e2bc2b5ae64
DevCraftProductions <DevCraftProductions@users.noreply.github.com>
2015-11-12 08:06:22 -0600

    Update AllPlayers.php

--- src/AllPlayers/AllPlayers.php
+++ src/AllPlayers/AllPlayers.php
@@ -1,3 +1,24 @@
 <?php
 
 namespace AllPlayers
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+
+class AllPlayers extends PluginBase implements Listener {
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("AllPlayers By ItMeDevCraft_ Loaded!");
+  }
+  
+  public function onPlayerChat(PlayerChatEvent $event) {
+    
+    $player = $event->getPlayer();
+    $message = str_replace("%p", $player->getName());
+    $message = str_replace("@a", $player->getName());
+  }
+}
+
+?>
